4ad1b56251f22b7568fd683763f9027f7f3717c3
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-02-04 15:43:13 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let username ;
+
+document.getElementById("mye").onclick=function(){
+    username=document.getElementById("my").value;
+    document.getElementById("p1").textContent = `hello ${username}`
+
+
+
+
+}
