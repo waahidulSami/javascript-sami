f7e375dd8849d888549a529a4fadce387be857c2
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-02-10 09:58:54 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,4 @@ let username ;
 document.getElementById("mye").onclick=function(){
     username=document.getElementById("my").value;
     document.getElementById("p1").textContent = `hello ${username}`
-
-
-
-
 }
